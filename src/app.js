d438c22732eda97118591150f5fa10400c9960d0
nzheng2000@yahoo.com <nzheng2000@yahoo.com>
2020-06-11 16:06:22 -0400

    test"wq!Please enter the commit message for your changes. Lines starting

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter';
 import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
@@ -12,21 +12,10 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-
-
- 
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-console.log("testing");
-
 const jsx = (
-    <Provider store={store}>
-      <AppRouter />
-    </Provider>
-  );
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
 ReactDOM.render(jsx, document.getElementById('app'));
